110beae96e673a6ef7230b8da2e989cb46712a37
TrinhChung <chungtrinh2k2@gmail.com>
2023-12-04 23:49:47 +0700

    add map from google map api

--- fe/src/index.js
+++ fe/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AuthProvider from "./providers/authProvider";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </APIProvider>
   </React.StrictMode>
 );
 
--- fe/src/pages/Guest/home/index.js
+++ fe/src/pages/Guest/home/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Map, Marker } from "@vis.gl/react-google-maps";
 
 const Home = () => {
-  return <div>Smart rent</div>;
+  const position = { lat: 53.54992, lng: 10.00678 };
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Map
+        center={position}
+        zoom={10}
+        mapId={process.env.REACT_APP_MAP_ID}
+      ></Map>
+    </div>
+  );
 };
 
 export default Home;
