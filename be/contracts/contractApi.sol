3d5b0a9503bba8e84e7d193dd82417fb609f7f9c
TrinhChung <chungtrinh2k2@gmail.com>
2023-12-26 23:02:44 +0700

    fix contract

--- be/contracts/contractApi.sol
+++ be/contracts/contractApi.sol
@@ -2,35 +2,26 @@
 pragma solidity ^0.8.9;
 
 contract SmartContract {
-
     struct Term {
         string name;
         string content;
     }
 
     struct ContractEntity {
+        uint256 id;
         uint32 renterId;
-
         uint32 sellerId;
-
         uint32 rentCost;
-
         uint32 duration;
-
-        uint256 timeStart;
-
-        uint256 paymentDeadline;
-
+        uint32 timeStart;
+        uint32 paymentDeadline;
         string payment_type;
-
         Term[] termArray;
-
         address renter;
-
         address seller;
     }
 
-    mapping(uint256=>ContractEntity) contracts;
+    mapping(uint256 => ContractEntity) contracts;
     ContractEntity[] contractArray;
 
     address owner;
@@ -39,23 +30,51 @@ contract SmartContract {
         owner = msg.sender;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function getProductById(uint256 _id) public view returns(ContractEntity memory) {
-        require(contracts[_id].renterId != 0 && (msg.sender == contracts[_id].renter || msg.sender == contracts[_id].seller));
+    function createSmartContract(
+        uint256 _id,
+        uint32 _renterId,
+        uint32 _sellerId,
+        uint32 _rentCost,
+        uint32 _duration,
+        uint32 _timeStart,
+        uint32 _paymentDeadline,
+        string memory _payment_type,
+        Term[] memory _termArray,
+        address _seller,
+        address _renter
+    ) public onlyOwner {
+        ContractEntity memory smartContract = ContractEntity(
+            _id,
+            _renterId,
+            _sellerId,
+            _rentCost,
+            _duration,
+            _timeStart,
+            _paymentDeadline,
+            _payment_type,
+            _termArray,
+            _seller,
+            _renter
+        );
 
-        return (contracts[_id]);
+        contracts[_id] = smartContract;
+        contractArray.push(smartContract);
     }
 
-    function getContractBySellerId() public view returns(ContractEntity[] memory) {
-         ContractEntity[] memory resContracts;
-         for (uint i = 1; i <= contractArray.length; i++) {
-            if (contractArray[i].seller == msg.sender) {
-                // resContracts.push(contractArray[i]);
-            }
-        }
+    function getSmartContractById(
+        uint256 _id
+    ) public view returns (ContractEntity memory) {
+        require(
+            contracts[_id].renterId != 0 &&
+                (msg.sender == contracts[_id].renter ||
+                    msg.sender == contracts[_id].seller)
+        );
+
+        return (contracts[_id]);
     }
 }
--- be/src/route/smartContract.js
+++ be/src/route/smartContract.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { handleCreateContract } from "../controllers/smartcontract";
+import { ethers } from "ethers";
 
 export const router = Router();
+
+const provider = new ethers.JsonRpcProvider(process.env.API_URL);
+const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 router.use("/", (req, res, next) => {
   return res.status(200).json({ status: 200 });
