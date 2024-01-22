d622e1340292b589f62f26b84d402116eca5c4a3
root <root@instance-1.us-central1-a.c.smartrental-407502.internal>
2024-01-22 16:24:08 +0000

    run docker in vps

--- be/src/controllers/auth.js
+++ be/src/controllers/auth.js
@@ -17,6 +17,7 @@ export const handleRegisterUser = async (req, res) => {
 export const handleLoginUser = async (req, res) => {
   try {
     const data = await loginUserService(req.body);
+    console.log(data)
     return res.status(200).json(data);
   } catch (error) {
     console.log(error);
