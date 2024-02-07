e6e58acd91823cfcde0e56db70a045d1fff08d48
TrinhChung <chungtrinh2k2@gmail.com>
2024-02-07 21:33:30 +0700

    only restart when update backend

--- be/checkFileChange.sh
+++ be/checkFileChange.sh
@@ -1,8 +1,12 @@
 files=`git diff --name-only -r HEAD^1 HEAD `
 checkChangeDb=false
 checkChangeSc=false
+changeBackend=false
 for file in $files; do
   echo $file
+  if [[ $file == *"be"* ]]; then
+     changeBackend = true
+  fi
   if [[ $file == *"be/contracts/contractApi.sol"* ]]; then
      checkChangeDb = true
   fi
@@ -11,11 +15,19 @@ for file in $files; do
   fi
 done
 
-if [[ $checkChangeDb == true ]]; then
+if [[ $checkChangeSc == true ]]; then
+     echo "Deploy and verify smart contract"
      npx hardhat run scripts/deploy.js --network testnet
      network=testnet ./verify.sh
 fi
 
-if [[ $checkChangeSc == true ]]; then
+if [[ $checkChangeDb == true ]]; then
+     echo "Migrate and seed data"
      ./migrate.sh
+fi
+
+if [[ $changeBackend == true ]]; then
+      echo "Restart backend"
+      docker compose down
+      docker compose up -d
 fi
