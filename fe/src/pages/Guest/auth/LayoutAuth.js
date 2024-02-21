9773aa3ab934aca47ec422fd5e4d256e2e660aca
TrinhChung <chungtrinh2k2@gmail.com>
2024-02-21 22:04:32 +0700

    fix layout auth

--- fe/src/pages/Guest/auth/LayoutAuth.js
+++ fe/src/pages/Guest/auth/LayoutAuth.js
@@ -3,19 +3,25 @@ import { Col, Row, Spin } from "antd";
 import "./Auth.scss";
 
 const LayoutAuth = ({ children, loading = false }) => {
+  const height = "543px";
+  const width = "500px";
+
   return (
     <Col span={24} className="login">
       <Spin spinning={loading}>
         <Row style={{ paddingBottom: 56, justifyContent: "center" }}>
           <Col
-            xs={24}
+            xs={8}
             xxl={8}
             style={{ display: "flex", justifyContent: "end" }}
           >
-            <Row className="background-banner-container">
+            <Row
+              className="background-banner-container"
+              style={{ height: height, width: width }}
+            >
               <Col span={24} className="background-banner-image" />
             </Row>
-            <Row className="image-banner-login" />
+            <Row className="image-banner-login" style={{ height: height }} />
           </Col>
           <Col xxl={6} className="form-auth">
             {children}
