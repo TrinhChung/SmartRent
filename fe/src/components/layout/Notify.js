0451a0fb3277f43876b520804044e8efa8f3e1e9
ngoctranbao <ngoc110904@gmail.com>
2024-03-02 12:23:15 +0700

    message notify

--- fe/src/components/layout/Notify.js
+++ fe/src/components/layout/Notify.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Row, Col } from "antd";
 import { useNavigate, useLocation } from "react-router-dom";
 import { faMessage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SocketContext } from "../../providers/socketProvider";
 import moment from "moment";
 import localization from "moment/locale/vi";
+import {
+  UpsertMessageNotify,
+} from "../../services/Notify";
 
 const Notify = ({ notify, key = 1 }) => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const { fetchNotifyOfUser } = useContext(SocketContext);
 
   const switchToLink = async (notify) => {
     var linkNotify;
@@ -17,9 +22,12 @@ const Notify = ({ notify, key = 1 }) => {
     }
 
     if (pathname !== linkNotify) {
-      navigate(`/room-chat/${notify.fkId}`);
-      // TODO xử lý lại sự kiện đọc
-      // Không cần thiết sử dụng socket hãy sử dụng post
+      notify.isRead = true;
+      const res = await UpsertMessageNotify(notify);
+      if (res.status === 200) {
+        navigate(`/room-chat/${notify.fkId}`);
+        await fetchNotifyOfUser();
+      }
     }
   };
 
@@ -37,12 +45,12 @@ const Notify = ({ notify, key = 1 }) => {
       className="notify-item"
     >
       <Col style={{ fontSize: 26 }}>
-        <FontAwesomeIcon icon={faMessage} />
+        {mappingIcon[notify?.type?notify.type:1]}
       </Col>
       <Col style={{ alignContent: "center", color: "var(--color-bold)" }}>
         <Row style={{}}>{notify.content}</Row>
         <Row style={{ paddingTop: 2, fontSize: 12, lineHeight: "12px" }}>
-          {moment(notify.time).fromNow()}
+          {moment(notify.createdAt).fromNow()}
         </Row>
       </Col>
     </Row>
