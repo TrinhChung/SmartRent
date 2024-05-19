c64fa6bbe23fc5d195b3182055f8172eb6c75a08
TrinhChung <chungtrinh2k2@gmail.com>
2024-05-20 01:14:17 +0700

    stop job test

--- be/src/cron-job/remindPayment.js
+++ be/src/cron-job/remindPayment.js
@@ -6,5 +6,5 @@ export const autoPayment = new cron.schedule("0 1 1 * * *", async function () {
 });
 
 export const testJob = new cron.schedule("*/10 * * * * *", async function () {
-  await handleGetUserPayment();
+  // await handleGetUserPayment();
 });
